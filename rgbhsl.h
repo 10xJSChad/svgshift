6c2e19f4a0a37385cc252262ed73304e14ed7169
10xJSChad <48174610+10xJSChad@users.noreply.github.com>
2024-12-24 08:28:24 +0100

    minor formatting correction

--- rgbhsl.h
+++ rgbhsl.h
@@ -215,7 +215,8 @@ HSL_clamp(struct HSL hsl)
 
 
 struct RGB
-RGB_greyscale(struct RGB rgb) {
+RGB_greyscale(struct RGB rgb) 
+{
     int grayscale = (int) (0.2126 * rgb.r + 0.7152 * rgb.g + 0.0722 * rgb.b);
     return (struct RGB) {grayscale, grayscale, grayscale};
 }
